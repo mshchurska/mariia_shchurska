191bed085365aa6ba206fde813bc733ba732981d
Mariia Shchurska <mariia.shchurska@uklon.com.ua>
2025-03-04 21:40:42 +0200

    Add calc

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+
 def add_number(a, b):
     return a + b
+
+def subtract_number(a, b):
+    return a - b
+
+
+
+print(add_number(a, b))
+print(subtract_number(a, b))
